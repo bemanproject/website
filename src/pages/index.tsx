d5ac95c6baf9cd98b78447af7b2d2c686b9eca0a
Radu Nichita <radunichita99@gmail.com>
2025-04-19 18:48:53 +0300

    Add main page
    Signed-off-by: Radu Nichita <radunichita99@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,30 +11,40 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx(styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+        <div className="hero__subtitle">
+          <div className={styles.taglineContainer}>
+            <img
+              src="./img/logo.png"
+              alt="Left Image"
+              style={{ width: '400px', height: '400px' }}
+            />
+            <span>{siteConfig.tagline}</span>
+            <img
+              src="./img/logo.png"
+              alt="Right Image"
+              style={{ width: '400px', height: '400px' }}
+            />
+          </div>
         </div>
+        <div className={styles.buttons}></div>
       </div>
     </header>
   );
 }
+
 
 export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Beman website/>"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
