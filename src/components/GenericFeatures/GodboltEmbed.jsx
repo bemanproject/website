04904e7ba262e388f211f95568c0203e395607f3
M. Galib Uludag <m231@tnctr.com>
2025-05-01 19:23:24 +0000

    add feature for embed godbolt links

---
+++ src/components/GenericFeatures/GodboltEmbed.jsx
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+const GodboltEmbed = ({ url, readOnly=false }) => {
+  const readOnlyParam = readOnly ? "readOnly=true&" : "";
+  const fullUrl = url.includes("godbolt.org/#")
+    ? url.replace(
+        "godbolt.org/#",
+        `godbolt.org/e?${readOnlyParam}hideEditorToolbars=false#`,
+      )
+    : url; // Keep short links unchanged
+
+  return (
+    <iframe
+      frameBorder="0"
+      style={{
+        margin: "auto",
+        display: "block",
+        overflow: "hidden",
+        height: "800px",
+        width: "90%",
+      }}
+      src={fullUrl}
+    ></iframe>
+  );
+};
+
+export default GodboltEmbed;
